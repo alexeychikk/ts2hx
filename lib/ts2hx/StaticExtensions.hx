9c9cc7300f1a940f4775de89fbd45127b12b0895
Alex Zinkevych <alexdarkmm@gmail.com>
2023-08-27 03:40:20 +0300

    fix: static extension methods now handle any value

--- lib/ts2hx/StaticExtensions.hx
+++ lib/ts2hx/StaticExtensions.hx
@@ -1,21 +1,21 @@
 package ts2hx;
 
 class StaticExtensions {
-  static public function or<T>(value: T, defaultValue: T): T {
+  static public function or(value: Any, defaultValue: Any): Any {
     switch (value) {
-      case 0, null, "", false, Math.NaN:
+      case 0, null, "", false, _ == Math.NaN => true:
         return defaultValue;
       case _:
         return value;
     }
   }
 
-  static public function and<T>(value: T, nextValue: T): T {
+  static public function and(value: Any, defaultValue: Any): Any {
     switch (value) {
-      case 0, null, "", false, Math.NaN:
+      case 0, null, "", false, _ == Math.NaN => true:
         return value;
       case _:
-        return nextValue;
+        return defaultValue;
     }
   }
 }
